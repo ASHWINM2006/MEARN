288e481f215047038bb62c840b43a349579d4885
ASHWINM2006 <ashwin20006@gmail.com>
2025-01-21 20:07:24 +0530

    Your commit message

---
+++ seceFrontend/src/App.jsx
@@ -0,0 +1,26 @@
+import Home from "./components/FunctionalComponents/Home";
+import About from "./components/FunctionalComponents/About";
+import ClassCompEG from "./components/classComponents/ClassCompEG";
+import Gallery from "./components/FunctionalComponents/Gallery";
+import Contact from "./components/FunctionalComponents/Contact";
+import Navbar from "./components/FunctionalComponents/Navbar";
+import Signup from "./components/FunctionalComponents/Signup";
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/About' element={<About/>}></Route>
+          <Route path="/Gallery" element={<Gallery image="SECE logo" page="Gallery"/>}></Route>
+          <Route path="/Contact" element={<Contact/>}></Route>
+          <Route path="/Signup" element={<Signup/>}></Route>
+          
+        </Routes>
+    </BrowserRouter>
+        )
+  }
+  
+  export default App;
